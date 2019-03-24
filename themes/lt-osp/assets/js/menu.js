50db0bf874a2a24a5445727b7ee3bd60e44724f4
Phill Sparks <me@phills.me.uk>
2019-03-24 21:52:22 +0000

    Toggle the menu on open or close
    This means that the focussed element is able to close the menu too, just like the visual element.

--- themes/lt-osp/assets/js/menu.js
+++ themes/lt-osp/assets/js/menu.js
@@ -4,27 +4,22 @@ $(function () {
 
     var mainMenu = document.getElementById("main-menu");
 
-    var menuToggles = document.getElementsByClassName("menu-toggle");
-    var expandMenu = function () {
-        mainMenu.setAttribute("aria-expanded", true);
+    var toggleMenu = function () {
+        var expanded = mainMenu.getAttribute("aria-expanded");
+        mainMenu.setAttribute("aria-expanded", expanded != "true");
     };
-    var onExpandMenu = function (evt) {
+    var onToggleMenu = function (evt) {
         evt.preventDefault();
-        expandMenu();
+        toggleMenu();
     };
+
+    var menuToggles = document.getElementsByClassName("menu-toggle");
     Array.from(menuToggles).forEach(function (button) {
-        button.addEventListener("click", onExpandMenu);
+        button.addEventListener("click", onToggleMenu);
     });
 
     var menuClosers = document.getElementsByClassName("menu-close");
-    var onCollapseMenu = function (evt) {
-        evt.preventDefault();
-        collapseMenu();
-    };
-    var collapseMenu = function () {
-        mainMenu.setAttribute("aria-expanded", false);
-    };
     Array.from(menuClosers).forEach(function (button) {
-        button.addEventListener("click", onCollapseMenu);
+        button.addEventListener("click", onToggleMenu);
     });
 });
