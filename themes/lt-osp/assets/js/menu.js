ba8291afe80f87b8927dd2bfb81f78503cd74539
Phill Sparks <me@phills.me.uk>
2019-04-22 15:22:48 +0100

    Show only one menu at a time

--- themes/lt-osp/assets/js/menu.js
+++ themes/lt-osp/assets/js/menu.js
@@ -4,6 +4,20 @@ $(function () {
 
     const ARIA_EXPANDED = "aria-expanded";
 
+    function forEach(list, callback) {
+        Array.prototype.forEach.call(list, callback);
+    }
+    function callEach(list) {
+        Array.prototype.forEach.call(list, function (callback) {
+            callback();
+        });
+    }
+
+    const CLOSERS = [];
+    var closeMenus = function () {
+        callEach(CLOSERS);
+    };
+
     /* Accounts for the height of a sticky banner at the top of the viewport */
     var scrollIntoView = function (elm) {
         elm.scrollIntoView(true);
@@ -13,27 +27,42 @@ $(function () {
         }
     };
 
+    /* Side Menu */
     var sideMenu = document.getElementById("side-menu");
 
     var toggleMenu = function () {
-        var expanded = sideMenu.getAttribute(ARIA_EXPANDED);
-        sideMenu.setAttribute(ARIA_EXPANDED, expanded != "true");
+        var isExpanded = sideMenu.getAttribute(ARIA_EXPANDED) == "true";
+        isExpanded ? closeMenu() : openMenu();
     };
+    var openMenu = function () {
+        closeMenus();
+        sideMenu.setAttribute(ARIA_EXPANDED, true);
+    };
+    var closeMenu = function () {
+        sideMenu.setAttribute(ARIA_EXPANDED, false);
+    }
+    CLOSERS.push(closeMenu);
     var onToggleMenu = function (evt) {
         if (!sideMenu) return;
 
         evt.preventDefault();
         toggleMenu();
     };
+    var onCloseMenu = function (evt) {
+        if (!sideMenu) return;
+
+        evt.preventDefault();
+        closeMenu();
+    }
 
     var menuToggles = document.getElementsByClassName("menu-toggle");
-    Array.from(menuToggles).forEach(function (button) {
+    forEach(menuToggles, function (button) {
         button.addEventListener("click", onToggleMenu);
     });
 
     var menuClosers = document.getElementsByClassName("menu-close");
-    Array.from(menuClosers).forEach(function (button) {
-        button.addEventListener("click", onToggleMenu);
+    forEach(menuClosers, function (button) {
+        button.addEventListener("click", onCloseMenu);
     });
 
     /* Help */
@@ -41,19 +70,21 @@ $(function () {
     var helpToggles = document.getElementsByClassName("help-toggle");
 
     var showHelp = function () {
-        Array.from(helpToggles).forEach(function (button) {
+        closeMenus();
+        forEach(helpToggles, function (button) {
             button.classList.add("active");
         });
         help.setAttribute(ARIA_EXPANDED, true);
         scrollIntoView(help);
     };
     var hideHelp = function () {
-        Array.from(helpToggles).forEach(function (button) {
+        forEach(helpToggles, function (button) {
             button.classList.remove("active");
             button.blur();
         });
         help.setAttribute(ARIA_EXPANDED, false);
     };
+    CLOSERS.push(hideHelp);
     var toggleHelp = function () {
         var isExpanded = help.getAttribute(ARIA_EXPANDED) == "true";
         isExpanded ? hideHelp() : showHelp();
@@ -65,7 +96,7 @@ $(function () {
         toggleHelp();
     };
 
-    Array.from(helpToggles).forEach(function (button) {
+    forEach(helpToggles, function (button) {
         button.addEventListener("click", onToggleHelp);
     });
 
@@ -74,19 +105,21 @@ $(function () {
     var searchToggles = document.getElementsByClassName("search-toggle");
 
     var showSearch = function () {
-        Array.from(searchToggles).forEach(function (button) {
+        closeMenus();
+        forEach(searchToggles, function (button) {
             button.classList.add("active");
         });
         search.setAttribute(ARIA_EXPANDED, true);
         scrollIntoView(search);
     };
     var hideSearch = function () {
-        Array.from(searchToggles).forEach(function (button) {
+        forEach(searchToggles, function (button) {
             button.classList.remove("active");
             button.blur();
         });
         search.setAttribute(ARIA_EXPANDED, false);
     };
+    CLOSERS.push(hideSearch);
     var toggleSearch = function () {
         var isExpanded = search.getAttribute(ARIA_EXPANDED) == "true";
         isExpanded ? hideSearch() : showSearch();
@@ -98,7 +131,7 @@ $(function () {
         toggleSearch();
     };
 
-    Array.from(searchToggles).forEach(function (button) {
+    forEach(searchToggles, function (button) {
         button.addEventListener("click", onToggleSearch);
     });
 
